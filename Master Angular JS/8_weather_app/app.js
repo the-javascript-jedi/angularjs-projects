c3f80243c9dbf48b37af0a53f8439b0774756d75
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-09-03 00:01:44 +0530

    made api call

--- Master Angular JS/8_weather_app/app.js
+++ Master Angular JS/8_weather_app/app.js
@@ -15,16 +15,51 @@ weatherApp.config(function ($routeProvider, $locationProvider) {
       controller: "forecastController",
     });
 });
+// SERVICES
+weatherApp.service("cityService", function () {
+  this.city = "chennai";
+});
 
 // CONTROLLERS
+// inject the custom city service (no need for $)
 weatherApp.controller("homeController", [
   "$scope",
   "$log",
-  function ($scope, $log) {},
+  "cityService",
+  function ($scope, $log, cityService) {
+    $scope.city = cityService.city;
+    // establish 2 way binding
+    $scope.$watch("city", function () {
+      cityService.city = $scope.city;
+    });
+  },
 ]);
 
 weatherApp.controller("forecastController", [
   "$scope",
-  "$log",
-  function ($scope, $log) {},
+  "$http",
+  "cityService",
+  function ($scope, $http, cityService) {
+    $scope.city = cityService.city;
+    // $scope.city = cityService.city || "London"; // Fallback city
+
+    const apiKey = "dab0c1237c271094b8379df4181ffa50";
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${$scope.city}&appid=${apiKey}&units=metric`;
+
+    // Only make the API call if $scope.city is defined
+    if ($scope.city) {
+      // make api call
+      $http.get(url).then(
+        function (response) {
+          console.log("response", response);
+          $scope.weatherResult = response.data;
+        },
+        function (error) {
+          console.error("Error:", error);
+        }
+      );
+    } else {
+      console.error("City name is undefined.");
+    }
+  },
 ]);
