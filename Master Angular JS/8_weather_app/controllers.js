19febce366dca482ac84cfac6086ccbedbb9e924
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-09-03 11:14:07 +0530

    added finishing

--- Master Angular JS/8_weather_app/controllers.js
+++ Master Angular JS/8_weather_app/controllers.js
@@ -35,92 +35,92 @@ weatherApp.controller("forecastController", [
     // Only make the API call if $scope.city is defined
     if ($scope.city) {
       // dummy api data
-      var testResponse = {
-        list: [
-          {
-            temp: {
-              day: 294,
-            },
-            dt: 1725300000,
-            main: {
-              temp: 293.49,
-              feels_like: 293.67,
-              temp_min: 293.2,
-              temp_max: 293.49,
-              pressure: 1010,
-              sea_level: 1010,
-              grnd_level: 1007,
-              humidity: 80,
-              temp_kf: 0.29,
-            },
-            weather: [
-              {
-                id: 803,
-                main: "Clouds",
-                description: "broken clouds",
-                icon: "04d",
-              },
-            ],
-            clouds: {
-              all: 75,
-            },
-            wind: {
-              speed: 3.46,
-              deg: 202,
-              gust: 5.91,
-            },
-            visibility: 10000,
-            pop: 0.39,
-            sys: {
-              pod: "d",
-            },
-            dt_txt: "2024-09-02 18:00:00",
-          },
-          {
-            temp: {
-              day: 295,
-            },
-            dt: 1725310800,
-            main: {
-              temp: 293.02,
-              feels_like: 293.2,
-              temp_min: 292.08,
-              temp_max: 293.02,
-              pressure: 1010,
-              sea_level: 1010,
-              grnd_level: 1007,
-              humidity: 82,
-              temp_kf: 0.94,
-            },
-            weather: [
-              {
-                id: 500,
-                main: "Rain",
-                description: "light rain",
-                icon: "10n",
-              },
-            ],
-            clouds: {
-              all: 83,
-            },
-            wind: {
-              speed: 2.78,
-              deg: 200,
-              gust: 5.9,
-            },
-            visibility: 10000,
-            pop: 0.44,
-            rain: {
-              "3h": 0.11,
-            },
-            sys: {
-              pod: "n",
-            },
-            dt_txt: "2024-09-02 21:00:00",
-          },
-        ],
-      };
-      $scope.weatherResult = testResponse;
+      //   var testResponse = {
+      //     list: [
+      //       {
+      //         temp: {
+      //           day: 294,
+      //         },
+      //         dt: 1725300000,
+      //         main: {
+      //           temp: 293.49,
+      //           feels_like: 293.67,
+      //           temp_min: 293.2,
+      //           temp_max: 293.49,
+      //           pressure: 1010,
+      //           sea_level: 1010,
+      //           grnd_level: 1007,
+      //           humidity: 80,
+      //           temp_kf: 0.29,
+      //         },
+      //         weather: [
+      //           {
+      //             id: 803,
+      //             main: "Clouds",
+      //             description: "broken clouds",
+      //             icon: "04d",
+      //           },
+      //         ],
+      //         clouds: {
+      //           all: 75,
+      //         },
+      //         wind: {
+      //           speed: 3.46,
+      //           deg: 202,
+      //           gust: 5.91,
+      //         },
+      //         visibility: 10000,
+      //         pop: 0.39,
+      //         sys: {
+      //           pod: "d",
+      //         },
+      //         dt_txt: "2024-09-02 18:00:00",
+      //       },
+      //       {
+      //         temp: {
+      //           day: 295,
+      //         },
+      //         dt: 1725310800,
+      //         main: {
+      //           temp: 293.02,
+      //           feels_like: 293.2,
+      //           temp_min: 292.08,
+      //           temp_max: 293.02,
+      //           pressure: 1010,
+      //           sea_level: 1010,
+      //           grnd_level: 1007,
+      //           humidity: 82,
+      //           temp_kf: 0.94,
+      //         },
+      //         weather: [
+      //           {
+      //             id: 500,
+      //             main: "Rain",
+      //             description: "light rain",
+      //             icon: "10n",
+      //           },
+      //         ],
+      //         clouds: {
+      //           all: 83,
+      //         },
+      //         wind: {
+      //           speed: 2.78,
+      //           deg: 200,
+      //           gust: 5.9,
+      //         },
+      //         visibility: 10000,
+      //         pop: 0.44,
+      //         rain: {
+      //           "3h": 0.11,
+      //         },
+      //         sys: {
+      //           pod: "n",
+      //         },
+      //         dt_txt: "2024-09-02 21:00:00",
+      //       },
+      //     ],
+      //   };
+      //   $scope.weatherResult = testResponse;
       // use this method to convert to fahrenheit
       $scope.convertToFahrenheit = function (degK) {
         return Math.round(1.8 * (degK - 273) + 32);
@@ -131,20 +131,20 @@ weatherApp.controller("forecastController", [
       };
 
       // make api call
-      // $http.get(url).then(
-      //   function (response) {
-      //     // console.log("response", response);
-      //     // display data based on days route params
-      //     let modifiedResponse = response;
-      //     modifiedResponse.data.list.splice($scope.days);
-      //     $scope.weatherResult = modifiedResponse.data;
-      //     // display all data
-      //     // $scope.weatherResult = response.data;
-      //   },
-      //   function (error) {
-      //     console.error("Error:", error);
-      //   }
-      // );
+      $http.get(url).then(
+        function (response) {
+          // console.log("response", response);
+          // display data based on days route params
+          let modifiedResponse = response;
+          modifiedResponse.data.list.splice($scope.days);
+          $scope.weatherResult = modifiedResponse.data;
+          // display all data
+          // $scope.weatherResult = response.data;
+        },
+        function (error) {
+          console.error("Error:", error);
+        }
+      );
     } else {
       console.error("City name is undefined.");
     }
