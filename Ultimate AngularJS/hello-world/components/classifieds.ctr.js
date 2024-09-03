199458404c51b467f67c70e261f3c97f4180c9f1
Nithin Samuel <nithin.thewebdev@gmail.com>
2024-09-03 17:56:44 +0530

    added form add submit

--- Ultimate AngularJS/hello-world/components/classifieds.ctr.js
+++ Ultimate AngularJS/hello-world/components/classifieds.ctr.js
@@ -6,13 +6,46 @@
     // inject the classifieds factory
     .controller(
       "classifiedsCtrl",
-      function ($scope, $http, classifiedsFactory) {
+      function ($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
         $scope.message = "AngularJS";
         // mock an api call
         classifiedsFactory.getClassifieds().then(function (data) {
           console.log("data", data);
           $scope.classifieds = data.data;
         });
+        // dummy contact
+        var contact = {
+          name: "Ryan Chenkie",
+          phone: "(555) 555 5555",
+          email: "ryanchenkie@gmail.com",
+        };
+
+        // sidenav button open
+        $scope.openSidebar = function () {
+          $mdSidenav("left").open();
+        };
+        // sidenav close
+        $scope.closeSidebar = function () {
+          $mdSidenav("left").close();
+        };
+        // save form data
+        $scope.saveClassified = function (classified) {
+          if (classified) {
+            // attach dummy contact to form data
+            classified.contact = contact;
+            $scope.classifieds.push(classified);
+            $scope.classified = {};
+            $scope.closeSidebar();
+            // show toast notification
+            $mdToast.show(
+              $mdToast
+                .simple()
+                .content("Classification Saved")
+                .position("top, right")
+                .hideDelay(3000)
+            );
+          }
+        };
       }
     );
 })();
